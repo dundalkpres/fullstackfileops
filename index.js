17f7c1a3a208906704cf07b13af77940d56922d5
Ron Wilson <rwilson1@underarmour.com>
2017-02-05 23:03:13 -0500

    finish up

--- index.js
+++ index.js
@@ -1,13 +1,53 @@
 /*eslint-env node*/
 'use strict';
 
-var Config = require('config');
-var rename = require('./lib/rename');
 var
-//	sourcePath = Config.get('sourcePath'),
+	fs = require('fs'),
+	path = require('path');
+
+var
+	Config = require('config'),
+	Bluebird = require('bluebird');
+
+var
+	renamer = require('./lib/renamer'),
+	editor = require('./lib/editor');
+
+var readdir = Bluebird.promisify(fs.readdir);
+
+var
+	sourcePath = Config.get('sourcePath'),
 	targetPath = Config.get('targetPath');
 
-console.log(targetPath);
+var
+	successes = [],
+	failures = [];
 
-rename([targetPath, 'a.txt'].join('/'), [targetPath, 'b.txt'].join('/'))
-	.catch(console.error);
+readdir(sourcePath)
+.catch(console.error)
+.then(function (files) {
+	return Bluebird.map(files, function (file) {
+		var filePath = path.join(sourcePath, file);
+		return editor.edit(filePath)
+			.then(function () {
+				return renamer.rename(filePath, targetPath);
+			})
+			.then(function () {
+				successes.push(filePath);
+			})
+			.catch(function (error) {
+				console.error(error);
+				failures.push(filePath);
+			});
+	}, {concurrency: 3});
+})
+.then(function () {
+	console.log([
+		'renamed',
+		successes.length,
+		'files, with',
+		failures.length,
+		'errors',
+		JSON.stringify(failures)
+	].join(' '));
+});
